4dd16fc1d2f9ee6b5b966027261936cf879d149f
Matheus <theobest9@gmail.com>
2020-06-03 16:21:38 -0300

    arquivos de banco de dados

---
+++ DB/admin_contratos.sql
@@ -0,0 +1,38 @@
+create table admin_contratos( 
+id int not null auto_increment primary key,
+
+#campo 1
+num_reg int not null,
+#/campo 1
+
+#campo 2
+tipo int(1) not null, #cada tipo de contrato tera um numero para identificação
+#/campo 2
+
+#campo 3
+partes varchar(255),
+#/campo 3
+
+#campo 4
+periodo int not null, #tempo restante de contrato em meses
+data_ass date not null, #data de assinatura de contrato
+data_term date, #data de termino automatico, pode ser omitida caso seja por execução de atividades
+#/campo 4
+
+#campo 5
+obj varchar(1000),
+#/campo 5
+
+#autenticação
+login varchar(64) not null,
+senha varchar(256) not null,
+#/autenticação
+
+cpf int(11) not null, #cpf para busca
+
+#campo 7
+estado int(1) not null, #cada estado tera um numero para identificação
+motivo varchar(255) #caso haja motivo para paralisação
+#/campo 7
+
+);
