e4d6cd826cf4fbaf248c546a954a5b5e5effdfe8
Emilio Vieira - Developer <hemiliodev@gmail.com>
2021-05-16 22:29:53 -0300

    Adaptacao de View Page-not-found para o sistema

---
+++ App/Views/home/page-not-found.php
@@ -0,0 +1,4 @@
+<!--Definindo o caminho do template(layout principal) e passando uma variavel pra ele "title"-->
+<?php $this->layout('main.template', ['title' => 'Página não encontrada']) ?>
+
+PÁGINA NÃO ENCONTRADA :()
